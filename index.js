ece72bb8c8b2af41275a4e64c4aaf7768443eb89
wiltondesigner <wiltondesigner@gmail.com>
2025-04-28 15:37:31 -0300

    atividade de classe

---
+++ index.js
@@ -0,0 +1,16 @@
+// const numero = Number(prompt('Digite um número'))
+
+
+// if (numero % 2 === 0) {
+//     alert('É par')
+// }else {
+//     alert('É ímpar')
+// }
+
+alert('Crie uma lista de tarefas')
+
+const tarefa = Number(prompt('digite uma tarefa')) 
+ 
+const tarefa['1','2','3','4','5','6'...]
+
+tarefa.push('Digite uma atividade')
