9f79e9c8b48d10ffe6ced5f1ecbee45dc7c3154e
Luanny <luanny>
2025-04-30 09:42:37 -0300

    Continuar exercício

---
+++ exercicio-condicao.js
@@ -0,0 +1,16 @@
+//### Aula 02 Lógica - Condicionais
+
+// **Exercício 1: Cálculo de Desconto**
+
+// Crie um programa que solicite o valor total de uma compra. 
+// Se o valor for maior que R$100, 
+// aplique um desconto de 10%. 
+// Exiba o valor final da compra com ou sem desconto.
+
+const valordaCompra = Number(prompt("Digite o valor de sua compra!"))
+
+if(valordaCompra>100) {
+    desconto de 10%
+}else{
+    valordaCompra
+}
